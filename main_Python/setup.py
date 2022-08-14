f7625a3b421fe61e394f6b01a505472b46d025fc
michael.vitucci3 <mikebvcc@gmail.com>
2022-08-14 05:23:49 -0500

    Bug Fix Continued

---
+++ main_Python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='main_Python',
+    version='3.9',
+    packages=[''],
+    url='',
+    license='',
+    author='mike',
+    author_email='',
+    description=''
+)
